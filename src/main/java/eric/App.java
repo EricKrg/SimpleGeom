45714211d3a7f37fc6bd0c2370154d0e994885c4
EricKrg <eric.krueger@uni-jena.de>
2018-11-16 10:17:18 +0100

    adjusted main

--- src/main/java/eric/App.java
+++ src/main/java/eric/App.java
@@ -1,6 +1,10 @@
 package eric;
 
 import javax.sound.sampled.Line;
+import java.nio.channels.Pipe;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Hello world!
@@ -10,16 +14,21 @@ public class App
 {
     public static void main( String[] args )
     {
-        Point first = new Point(1,1);
-        Point second = new Point(3,3);
+        Point first = new Point(1,4);
+        Point second = new Point(7,3);
         Point on = new Point(2,3);
         Point up = new Point(1,3);
+        List<Point> points = Arrays.asList(first,second,on);
+        PolyLine mline = new PolyLine(points);
+        System.out.println(mline.getLength());
+        /*
         LineSegment seg = new LineSegment(first,second);
         LineSegment upper = new LineSegment(up, on);
         System.out.println(seg.inBounds(upper));
         System.out.println("is on " + seg.intersect(on));
         System.out.println(seg.getLength());
         System.out.println(seg.intersect(upper));
+        */
     }
 
 }
