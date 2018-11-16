d1df00c124decdca521f62f7e53d567e82ee9cfc
EricKrg <eric.krueger@uni-jena.de>
2018-11-16 10:16:23 +0100

    polyline base

---
+++ src/main/java/eric/PolyLine.java
@@ -0,0 +1,32 @@
+package eric;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class PolyLine {
+    List<Point> points;
+    List<LineSegment> lines;
+    public PolyLine(List<Point> pointList) {
+        this.points = pointList;
+        this.lines = new ArrayList<>(pointList.size()); // init array size
+        Point start = this.points.get(0);
+        for(Point p : pointList){
+            if(start == p){ continue; }
+            else {
+                LineSegment temp = new LineSegment(start, p);
+                this.lines.add(temp);
+                start = p;
+                temp.print(); // checking start end coords
+            }
+        }
+    }
+    public double getLength() {
+        double sum = 0;
+        for(LineSegment l : this.lines){
+            sum = sum + l.getLength();
+        }
+        return sum;
+    }
+}
