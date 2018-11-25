835fa37265af1990522089210f6aa57b4667c6f2
EricKrg <eric.krueger@uni-jena.de>
2018-11-25 17:33:31 +0100

    added unit tests

--- src/test/java/eric/TestLineSegment.java
+++ src/test/java/eric/TestLineSegment.java
@@ -8,9 +8,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestLineSegment {
     Point a = new Point(1,1);
     Point b = new Point(2,1);
+    Point c = new Point(1, 0);
+    Point d = new Point(1,3);
     LineSegment testL = new LineSegment(a,b);
+    LineSegment testC = new LineSegment(a,c);
+    LineSegment testD = new LineSegment(a,d);
+
     @Test
-    public void TestgetLen(){
+    public void testgetLen(){
         assertEquals(1.0, testL.getLength());
+        assertEquals(1.0, testC.getLength());
+        assertEquals(2.0, testD.getLength());
     }
 }
